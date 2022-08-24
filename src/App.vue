1307189c97ecfadc07c9a77f00a8223239b5fd82
hyunwoo Lee <leehwoo9797@gmail.com>
2022-08-24 17:30:33 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-main>
+      <Splash :isLoading="isLoading" />
+      <router-view v-if="!isLoading" />
+    </v-main>
+    <MyFooter />
+  </v-app>
+</template>
+
+<script>
+import MyFooter from "./components/BottomMenu.vue";
+import Splash from "./components/Splash.vue"; //로드
+
+export default {
+  name: "App",
+
+  components: {
+    MyFooter,
+    Splash,
+  },
+
+  data: () => ({
+    //
+    isLoading: true,
+  }),
+
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2500);
+  },
+};
+</script>
